b9e7209065303b050849082ea644cef1b0cb7036
Damianos Melidis <melidis@l3s.de>
2020-04-27 12:38:28 +0200

    Test for known ORF sequences from virulign github
    - check alignment only for first sequence
    
    Test: PASS

--- code/main.py
+++ code/main.py
@@ -18,15 +18,46 @@ from MFA2CSV import MFA2CSV
 ###                    ###
 ###     SARS-Cov2      ###
 ###                    ###
+
+#############
+##  Test   ##
+#############
+#use as test orf the ORF7b for SARS-cov2 from virualign and create 3 sequences with the following variants:
+#>Ref
+#CCCATTAGCCCTATTGAGACTGTACCAGTAAAATTAAAGCCAGGAATGGATGGCCCAAAA
+#>Seq1
+#------------ATTGACACTGTACCAGTAACATTAAAGCCAGGAATGGATGGACCAAAG
+#>Seq2
+#CCTATG---------GAAACTGTGCCAGTAAAATTAAAGCCAGGAATGGAT--------- (not displayed)
+#>Seq3
+#CTCATTAGTCCTATTAGT---------GTAAAATTAAAACCAGGAATGGATGGCCCAAGG  (not displayed)
+#>Seq4
+#------AGTCCCATTGAAACTGTACCAGTAAAA---------GGA---GATGGCCCAAAG (not displayed)
+data_path = "/home/damian/Documents/L3S/projects/sars_cov2/data"
+alignments_folder = "test_alignments"
+xmls_folder = "test_orf_xml"
+#ORF input:
+mfa_file = "test_mfa_nt.fasta"
+orf_xml_file = "test_orf.xml"
+ncbi_ref_id = "test_ref"
+#output
+out_path = "/home/damian/Documents/L3S/projects/sars_cov2/test_variants"
+
+"""
 # example for S (Spike) protein and 2 target sequences
+#inputs:
 data_path = "/home/damian/Documents/L3S/projects/sars_cov2/data"
 alignments_folder = "alignments"
 xmls_folder = "xmls"
-out_path = "/home/damian/Documents/L3S/projects/sars_cov2/variants"
-
+#ORF input:
 mfa_file = "S_mfa_nt.fasta"
 orf_xml_file = "S.xml"
 ncbi_ref_id = "NC_045512.2"
+
+#output:
+out_path = "/home/damian/Documents/L3S/projects/sars_cov2/variants"
+"""
+
 print("=== ~ ===")
 MFA2CSV_S_protein_test = MFA2CSV(data_path, alignments_folder, xmls_folder, ncbi_ref_id, out_path)
 MFA2CSV_S_protein_test.run(orf_xml_file, mfa_file)

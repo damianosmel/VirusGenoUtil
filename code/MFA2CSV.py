bab8061efb281356b70009326cfe99435c85ea3a
Damianos Melidis <melidis@l3s.de>
2020-04-30 23:35:21 +0200

    Feature: add absolute reference position + sort by position final variant csv
    Test: run for 10 NCBI target sequences compared to reference - PASS.

--- code/MFA2CSV.py
+++ code/MFA2CSV.py
@@ -1,9 +1,12 @@
-from Bio import AlignIO
+from Bio import AlignIO,SeqIO,Entrez
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import xml.etree.ElementTree as ET
 from os.path import join, exists
 from os import scandir
+from pandas import read_csv
+import urllib
+import time
 from utils import create_dir, is_fasta_file_extension, extract_orf_name
 
 
@@ -50,6 +53,10 @@ class MFA2CSV:
 		self.xmls_path = join(data_path, xmls_folder)
 		self.ref_id = ncbi_ref_id
 		self.ref_record = None
+		self.entrez_email = open(join(data_path, "email.txt")).read().strip()
+		Entrez.email = self.entrez_email
+		print("Loaded email: {}".format(self.entrez_email))
+		self.gene_annots = {}
 		create_dir(out_path)
 
 	def parse_ref_xml(self, xml_file):
@@ -72,9 +79,11 @@ class MFA2CSV:
 		self.ref_seq = Seq(root.attrib["referenceSequence"])
 		ref_name, ref_description = None, None
 		if "name" in root.attrib.keys():
-			ref_name = root.attrib["name"]
+			ref_name = root.attrib["name"].strip()
+			if "orf" in ref_name:
+				ref_name = "ORF" + ref_name.split("orf")[1]
 		if "description" in root.attrib.keys():
-			ref_description = root.attrib["description"]
+			ref_description = root.attrib["description"].strip()
 
 		self.ref_record = SeqRecord(Seq(root.attrib["referenceSequence"]),
 		                            id=self.ref_id, name=ref_name,
@@ -82,6 +91,70 @@ class MFA2CSV:
 
 		print("Parsed reference record:\n {}".format(self.ref_record))
 		print("---")
+
+	def fetch_genbank(self, seq_id):
+		"""
+		Fetch target sequence genbank from NCBI Entrez
+
+		Parameters
+		----------
+		seq_id : str
+			sequence NCBI id
+
+		Returns
+		-------
+		file handle
+			fetched genbank handle
+		"""
+		max_trials = 3
+		while max_trials > 0:
+			try:
+				print("Fetch genbank")
+				genbank_handle = Entrez.efetch(db="nucleotide", id=seq_id, rettype="gb", retmode="xml")
+				retry = False
+			except urllib.error.HTTPError as err:
+				if err.code == 500 or err.code == 504:
+					# server error or proxy error
+					print("re-try")
+					retry = True
+				else:
+					raise
+			if retry:
+				max_trials -= 1
+				time.sleep(20)
+			else:
+				max_trials = 0
+		return genbank_handle
+
+	def parse_genbank(self, seq_id, genbank_handle):
+		"""
+		Parse fetched genbank handle
+
+		Parameters
+		----------
+		seq_id : str
+			sequence NCBI id
+		genbank_handle: file handle
+			fetched genbank handle
+
+		Returns
+		-------
+		None
+		"""
+		print("Parse fetched genbank")
+		self.gene_annots[seq_id] = {}
+		for rec in Entrez.read(genbank_handle):
+			print("for record")
+			annotations = rec["GBSeq_feature-table"]
+			for annotation in annotations:
+				if annotation["GBFeature_key"] == "gene":
+					gene_name = annotation["GBFeature_quals"][0]["GBQualifier_value"]
+					location = annotation["GBFeature_location"].split("..")
+					if gene_name not in self.gene_annots[seq_id]:
+						self.gene_annots[seq_id][gene_name] = {"start": int(location[0]), "end": int(location[1])}
+
+		print("Successfully loaded the following gene annotations: ")
+		print(self.gene_annots[seq_id])
 
 	def alignment2csv(self, alignment):
 		"""
@@ -99,6 +172,11 @@ class MFA2CSV:
 		None
 		"""
 		print("Convert alignment of target id {} to variants csv".format(alignment.id))
+		if self.ref_record.id not in self.gene_annots:
+			print("Fetch and then parse genbank annotations for target id: {}".format(alignment.id))
+			fetched_genbank = self.fetch_genbank(self.ref_record.id)
+			self.parse_genbank(self.ref_record.id, fetched_genbank)
+
 		variants_name = alignment.id + "_variants.csv"
 		if not exists(join(self.out_path, variants_name)):
 			print("Create file: {}".format(variants_name))
@@ -106,13 +184,33 @@ class MFA2CSV:
 				variants_out.write("ORF,POS,REF_ID,TARGET_ID,REF,ALT\n")
 		with open(join(self.out_path, variants_name), "a") as variants_out:
 			print("Update file: {}".format(variants_name))
+			orf_start_pos = self.gene_annots[self.ref_record.id][self.ref_record.name]["start"]
 			for ref_pos, ref_base in enumerate(self.ref_record.seq):
 				if ref_base != alignment.seq[ref_pos]:  # variant detected
 					print("Write variant")
 					variant_row = ",".join(
-						[self.ref_record.name, str(ref_pos), self.ref_record.id, alignment.id, ref_base,
+						[self.ref_record.name, str(orf_start_pos + ref_pos), self.ref_record.id, alignment.id, ref_base,
 						 alignment.seq[ref_pos]])
 					variants_out.write(variant_row + "\n")
+
+	def sort_variants(self):
+		"""
+		Sort variants csv
+
+		Parameters
+		----------
+
+		Returns
+		-------
+		None
+		"""
+		print("Sort variants csvs")
+		with scandir(self.out_path) as out_dir:
+			for out_content in out_dir:
+				if out_content.name.endswith(".csv") and out_content.is_file():
+					variants_df = read_csv(join(self.out_path,out_content.name), sep=",", header=0)
+					variants_df.sort_values(by="POS",ascending=True,inplace=True)
+					variants_df.to_csv(join(self.out_path,out_content.name),sep=",",index=False)
 
 	def run(self, xml_file, mfa_file):
 		"""
@@ -149,6 +247,7 @@ class MFA2CSV:
 
 		Returns
 		-------
+		None
 		"""
 		with scandir(self.alignments_path) as alignments_dir:
 			for alignments_content in alignments_dir:
@@ -161,3 +260,4 @@ class MFA2CSV:
 					for alignment in multiple_alignment:
 						self.alignment2csv(alignment)
 					print("---")
+		self.sort_variants()

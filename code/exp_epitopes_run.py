d9f63314ff89c4eead6e762a7a9ea0760b531d41
Damianos Melidis <melidis@l3s.de>
2020-06-19 17:59:18 +0200

    Update code for T-cells epitopes
     - function to retrieve epitopes from IEDB assay csv
     - function to map epitopes to HLA info
     - function to map epitopes to their location in the protein of provenance
    
    Test: run current code for P59594.fasta - PASS

--- code/exp_epitopes_run.py
+++ code/exp_epitopes_run.py
@@ -13,6 +13,7 @@ from code.utils import create_dir
 ###                                          ###
 ### B cells epitopes sars-cov1 --> sars-cov2 ###
 ###                                          ###
+"""
 # input
 data_path = "/home/damian/Documents/L3S/projects/sars_cov2/data/exp_epitopes_input"
 query_proteins_folder = "sars_cov1_proteins"  # please name _Bcells
@@ -22,20 +23,21 @@ exp_epitopes_folder = exp_epitopes_folder
 window_size = 10
 save_plot = True
 sliding_avg_cutoff = 0.3
-save_at_min_RF = True
+split_at_min_RF = True
 save_immunodominant_reg = True
+process_tcells = False
 # output
 out_path = "/home/damian/Documents/L3S/projects/sars_cov2/exp_epitopes/Bcells"
 create_dir(out_path)
 
-"""
+
 print("==== ====")
 Immunodominance_Bcells = Immunodominance(data_path, query_proteins_folder, exp_epitopes_folder, assay_species, out_path)
-processed_proteins = Immunodominance_Bcells.process_all_proteins(window_size, save_plot, sliding_avg_cutoff,
-                                                                 save_at_min_RF,
+processed_proteins = Immunodominance_Bcells.process_all_proteins(process_tcells, window_size, save_plot, sliding_avg_cutoff,
+                                                                 split_at_min_RF,
                                                                  save_immunodominant_reg)
 """
-
+"""
 print("==== ====")
 # input
 subject_proteins_folder = "sars_cov2_proteins"
@@ -45,30 +47,30 @@ ncbi_ids = {"relative_organism": "NC_004718.3", "target_organism": "NC_045512.2"
 HomologyBasedEpitopes_Bcells = HomologyBasedEpitopes(ncbi_ids, data_path, subject_proteins_folder, blast_folder,
                                                      out_path)
 HomologyBasedEpitopes_Bcells.find_epitopes()
-
 """
+
 ###                                          ###
 ### T cells epitopes sars-cov1 --> sars-cov2 ###
 ###                                          ###
 # input
 data_path = "/home/damian/Documents/L3S/projects/sars_cov2/data/exp_epitopes_input"
-query_proteins_folder = "sars_cov1_proteins"
+query_proteins_folder = "sars_cov1_proteins_Tcells"
 exp_epitopes_folder = "Tcells"
 assay_species = "human"
-exp_epitopes_folder = exp_epitopes_folder
 window_size = 10
 save_plot = True
 sliding_avg_cutoff = 0.3
+split_at_min_RF = True
 save_immunodominant_reg = True
+process_tcells = True
 # output
 out_path = "/home/damian/Documents/L3S/projects/sars_cov2/exp_epitopes/Tcells"
 create_dir(out_path)
 
-# print("==== ====")
-# Immunodominance_Tcells = Immunodominance(data_path, query_proteins_folder, exp_epitopes_folder, assay_species, out_path)
-# processed_proteins = Immunodominance_Tcells.process_all_proteins(window_size, save_plot, sliding_avg_cutoff,
-#                                                                  save_immunodominant_reg)
-
+print("==== ====")
+Immunodominance_Tcells = Immunodominance(data_path, query_proteins_folder, exp_epitopes_folder, assay_species, out_path)
+processed_proteins = Immunodominance_Tcells.process_all_proteins(process_tcells, window_size, save_plot, sliding_avg_cutoff, split_at_min_RF, save_immunodominant_reg)
+"""
 print("==== ====")
 # input
 subject_proteins_folder = "sars_cov2_proteins"

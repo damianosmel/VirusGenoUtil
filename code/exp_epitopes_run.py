c95426e9275911219ebada42d071927ae7f860d4
Damianos Melidis <melidis@l3s.de>
2020-06-26 19:38:35 +0200

    T-cells run:
     - after blasting sars cov1 epitopes to sars cov2 proteins
     - create csv file with homology epitopes
    
    Test: run for all sars-cov2 proteins - PASS

--- code/exp_epitopes_run.py
+++ code/exp_epitopes_run.py
@@ -26,6 +26,7 @@ sliding_avg_cutoff = 0.3
 split_at_min_RF = True
 save_immunodominant_reg = True
 process_tcells = False
+
 # output
 out_path = "/home/damian/Documents/L3S/projects/sars_cov2/exp_epitopes/Bcells"
 create_dir(out_path)
@@ -70,13 +71,15 @@ process_tcells = True
 out_path = "/home/damian/Documents/L3S/projects/sars_cov2/exp_epitopes/Tcells"
 create_dir(out_path)
 
+"""
 print("==== ====")
 Immunodominance_Tcells = Immunodominance(data_path, query_proteins_folder, exp_epitopes_folder, assay_species, out_path)
 processed_proteins = Immunodominance_Tcells.process_all_proteins(process_tcells, window_size, save_plot,
                                                                  sliding_avg_cutoff, split_at_min_RF,
                                                                  save_immunodominant_reg)
-
 """
+
+
 print("==== ====")
 # input
 subject_proteins_folder = "sars_cov2_proteins"
@@ -85,7 +88,7 @@ ncbi_ids = {"relative_organism": "NC_004718.3", "target_organism": "NC_045512.2"
 
 HomologyBasedEpitopes_Tcells = HomologyBasedEpitopes(ncbi_ids, data_path, subject_proteins_folder, blast_folder, out_path)
 HomologyBasedEpitopes_Tcells.find_epitopes(process_tcells)
-"""
+
 
 print("=== * ===")
 print("== *** ==")

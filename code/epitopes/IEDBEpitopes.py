00159b34421ffef144be9e2e011586e4c4dff6ae
Damianos Melidis <melidis@l3s.de>
2020-07-16 17:26:05 +0200

    Feature: Import IEDB
     - start IEDB epitopes class and its run file
    
    Status: Developing

---
+++ code/epitopes/IEDBEpitopes.py
@@ -0,0 +1,29 @@
+class IEDBEpitopes:
+	"""
+	Class to extract B and T-cells epitopes for
+	each virus taxon id found in virus_protein folder
+
+	Input epitopes per cell type are downloaded from http://www.iedb.org/database_export_v3.php
+	IEDB paper reference:
+	Vita R, Mahajan S, Overton JA, Dhanda SK, Martini S, Cantrell JR, Wheeler DK, Sette A, Peters B.
+	The Immune Epitope Database (IEDB): 2018 update. Nucleic Acids Res. 2019 Jan 8;47(D1):D339-D343.
+	doi: 10.1093/nar/gky1006. PMID: 30357391
+	"""
+
+	def __init__(self):
+		pass
+
+	def export_csv(self):
+		pass
+
+	def process_all_viruses(self):
+		pass
+
+	def process_virus_proteins(self):
+		pass
+
+	def process_Bcells(self):
+		pass
+
+	def process_Tcells(self):
+		pass
---
+++ code/iedb_epitopes_run.py
@@ -0,0 +1,13 @@
+###                                                          ###
+###                  Import IEDB epitopes                    ###
+###                                                          ###
+
+###                                                          ###
+###     input = IEDB Bcells.csv + Tcells.csv                 ###
+###     output = epitope csv per taxon id                    ###
+###                                                          ###
+
+# input
+data_path = "/home/damian/Documents/L3S/projects/sars_cov2/iedb_data"
+cells_epitopes_folder = "cell_epitopes"
+virus_proteins_folder = "virus_proteins"
